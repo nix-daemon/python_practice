32a2c9105f499a70215653a09e29985b1f71353d
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-01 13:49:07 -0500

    Commenting out some lines for modules

--- car.py
+++ car.py
@@ -66,29 +66,29 @@ class Battery:
 			range = 315
 		print(f"This car can go about {range} miles on a full charge.")
 
-my_new_car = Car('audi', 'a4', 2019)
+# #my_new_car = Car('audi', 'a4', 2019)
 
-print(my_new_car.get_descriptive_name())
+# print(my_new_car.get_descriptive_name())
 
-my_new_car.update_odometer(25)
-my_new_car.read_odometer()
+# my_new_car.update_odometer(25)
+# my_new_car.read_odometer()
 
-my_new_car.odometer_reading = 20
-my_new_car.read_odometer()
+# my_new_car.odometer_reading = 20
+# my_new_car.read_odometer()
 
-my_new_car.update_odometer(21)
+# my_new_car.update_odometer(21)
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
 
-my_used_car.increment_odometer(-100)
-my_used_car.read_odometer()
+# my_used_car.increment_odometer(-100)
+# my_used_car.read_odometer()
 
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.fill_gas_tank()
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla = ElectricCar('tesla', 'model s', 2019)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.fill_gas_tank()
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
