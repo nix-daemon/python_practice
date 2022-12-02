bdd6ccbd38e5b21af13b052d2b9cd122bfe96292
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-02 06:52:27 -0500

    Commented out print functions for importing

--- restaurant.py
+++ restaurant.py
@@ -12,25 +12,25 @@ class Restaurant:
 	def open_restuarant(self):
 		print(f"{self.restaurant_name} is now open!!")
 
-restaurant_1 = Restaurant('Subway', 'Subs')
-restaurant_2 = Restaurant('Takosushi', 'Sushi')
-restaurant_3 = Restaurant('Mellow Mushroom', 'Pizza')
-#restaurants = [restaurant_1, restaurant_2, restaurant_3]
+# restaurant_1 = Restaurant('Subway', 'Subs')
+# restaurant_2 = Restaurant('Takosushi', 'Sushi')
+# restaurant_3 = Restaurant('Mellow Mushroom', 'Pizza')
+# #restaurants = [restaurant_1, restaurant_2, restaurant_3]
 
-print(restaurant_1.restaurant_name)
-print(restaurant_1.cuisine_type)
-restaurant_1.describe_restaurant()
-restaurant_1.open_restuarant()
-print("\n")
-print(restaurant_2.restaurant_name)
-print(restaurant_2.cuisine_type)
-restaurant_2.describe_restaurant()
-restaurant_2.open_restuarant()
-print("\n")
-print(restaurant_3.restaurant_name)
-print(restaurant_3.cuisine_type)
-restaurant_3.describe_restaurant()
-restaurant_3.open_restuarant()
+# print(restaurant_1.restaurant_name)
+# print(restaurant_1.cuisine_type)
+# restaurant_1.describe_restaurant()
+# restaurant_1.open_restuarant()
+# print("\n")
+# print(restaurant_2.restaurant_name)
+# print(restaurant_2.cuisine_type)
+# restaurant_2.describe_restaurant()
+# restaurant_2.open_restuarant()
+# print("\n")
+# print(restaurant_3.restaurant_name)
+# print(restaurant_3.cuisine_type)
+# restaurant_3.describe_restaurant()
+# restaurant_3.open_restuarant()
 
 #for restaurant in restaurants:
 #	print(restaurant.restaurant_name)
