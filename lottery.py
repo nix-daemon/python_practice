0f5a84a810f650461ba390580a61ee0967f5dc62
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-02 09:02:13 -0500

    Modeling a lottery drawing

---
+++ lottery.py
@@ -0,0 +1,23 @@
+from random import choice
+
+i = 0
+l = 0
+win = 0
+winner = []
+my_ticket = ['a', 'p', 4, 48]
+
+lottery_pool = ['a', 't', 'p', 'w','l', 13, 4, 48, 62, 857, 0, 56, 3, 5, 9]
+
+while winner != my_ticket:
+    if i < 4:
+        winner.append(choice(lottery_pool))
+        i += 1
+    else:
+        print('Any ticket with the following 4 numbers wins:')
+        for num in winner:
+            print(num)            
+        winner.clear()
+        i = 0
+        l += 1
+        print(l)
+print(f"It took {l} number of attempts to win!")
