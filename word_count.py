630b388b4d5f0a0a810ff42c79b92742a170dfe2
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-03 12:23:15 -0500

    Create word_count.py

---
+++ word_count.py
@@ -0,0 +1,16 @@
+def count_words(filename):
+    """Count the approximate number of words in a file."""
+    try:
+        with open(filename, encoding = 'utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+
+filenames = ['text_files/alice.txt', 'text_files/moby_dick.txt', 
+'siddhartha.txt', 'text_files/little_women.txt']
+for filename in filenames:
+    count_words(filename)
