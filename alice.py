bdb66dd3a2dcfceeae5512b62a950f7d01418e37
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-03 12:08:48 -0500

    Create alice.py

---
+++ alice.py
@@ -0,0 +1,12 @@
+filename = 'text_files/alice.txt'
+
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
+else:
+    # Count the approximate number of words in the file.
+    words = contents.split()
+    num_words = len(words)
+    print(contents)
