a3bfe05b5e96321acce46a4dae7ad7819cafbb52
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-04 06:27:44 -0500

    Create addition_calculator.py

---
+++ addition_calculator.py
@@ -0,0 +1,10 @@
+while True:
+    print("Enter two numbers and I will give you their sum.")
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+    try:
+        sum = int(a) + int(b)
+    except ValueError:
+        print("I can only add two integers together!")
+    else:
+        print(sum)
