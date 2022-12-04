b007b0ed72a495c330c442eb6f7c15547bd4f329
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-04 06:27:36 -0500

    Remove while loop

--- addition.py
+++ addition.py
@@ -1,10 +1,9 @@
-while True:
-    print("Enter two numbers and I will give you their sum.")
-    a = input("Enter the first number: ")
-    b = input("Enter the second number: ")
-    try:
-        sum = int(a) + int(b)
-    except ValueError:
-        print("I can only add two integers together!")
-    else:
-        print(sum)
+print("Enter two numbers and I will give you their sum.")
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+try:
+    sum = int(a) + int(b)
+except ValueError:
+    print("I can only add two integers together!")
+else:
+    print(sum)
