187c73793c869999cc2fd1f07bf04276724eb420
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-05 08:31:31 -0500

    Refactored into smaller tasks

--- remember_me.py
+++ remember_me.py
@@ -1,15 +1,31 @@
 import json
 
-# Load the username, if it has been stored previously.
-# Otherwise, prompt for the username and store it.
-filename = 'json/username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'json/username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
     username = input("What is your name?")
-    with open(filename, w) as f:
+    filename = 'json/username.json'
+    with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
         print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back {username}!")
+
+greet_user()
