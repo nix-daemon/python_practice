9a555cba9954bb07e4a6fdb2f237de4868aaa6aa
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-05 14:18:07 -0500

    Refactored

--- favorite_number.py
+++ favorite_number.py
@@ -1,3 +1,7 @@
-number = 4
-message = f"My favorite number is {number}!"
-print(message)
+import json
+
+num = int(input("Please enter your favorite number: ")
+filename = 'json/number.json'
+
+with open(filename, 'w') as f:
+    json.dump(num, f)
