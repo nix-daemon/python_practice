f7c451dafe9ab29ddcf6a90acc2bab575c25489b
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-06 20:56:23 -0500

    Create test_cities.py

---
+++ test_cities.py
@@ -0,0 +1,18 @@
+import unittest
+from city_functions import city_function
+
+class CitiesTestCase(unittest.TestCase):
+    """Tests for 'city_functions.py'."""
+
+    def test_city_country(self):
+        """Do cities like Fort Worth work."""
+        location = city_function('fort worth', 'usa')
+        self.assertEqual(location, 'Fort Worth, Usa')
+
+    def test_city_country_population(self):
+        """Do cities like Fort Worth work with population"""
+        location = city_function('fort worth', 'usa', 50000)
+        self.assertEqual(location, 'Fort Worth, Usa - Population 50000')
+
+if __name__ == '__main__':
+     unittest.main()
