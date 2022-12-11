16d991193b7c36d96d7afbb2cc0141f4f77e396b
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-11 05:54:42 -0500

    Create fly.py

---
+++ fly.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Rocket:
+    """A class to manage the rocket"""
+
+    def __init__(self, rc_game):
+        """Initialize the ship and set its starting postiion."""
+        self.screen = rc_game.screen
+        self.screen_rect = rc_game.screen.get_rect()
+
+        # Load the rocket image and get its rect.
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new rocket at the center of the screen.
+        self.rect.center = self.screen_rect.center
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False 
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= 1
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += 1
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
