dfa63afefaa99178b50f8b2eb9873ec67ada8e6b
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-11 05:54:46 -0500

    Create rocket.py

---
+++ rocket.py
@@ -0,0 +1,58 @@
+import pygame
+import sys
+
+from fly import Rocket
+
+class RocketControl:
+    """Overall class to manage game"""
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Rocket Control")
+        self.bg_color = (255, 255, 255)
+        self.rocket = Rocket(self)
+
+    def run_game(self):
+        """Start the main loop of the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.rocket.update()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.rocket.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.rocket.moving_down = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.rocket.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.rocket.moving_down = False
+            
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.bg_color)
+        self.rocket.blitme()
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    rc = RocketControl()
+    rc.run_game()
