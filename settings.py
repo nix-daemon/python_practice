3d96b32118f4758ee46b9636c8fa4b111168d741
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-18 07:20:41 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store alls ettings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the games settings."""
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Bullet settings
+        self.bullet_speed= 1.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
